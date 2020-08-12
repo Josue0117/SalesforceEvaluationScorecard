ddd77ade2c39b1ab9ff2288ca0b2c69bac101cd8
Josue0117 <69014913+Josue0117@users.noreply.github.com>
2020-08-12 16:06:33 -0500

    Add files via upload

--- SalesforceEvaluationCoreCard/force-app/main/default/aura/skillEvaluationAndComents/skillEvaluationAndComentsController.js
+++ SalesforceEvaluationCoreCard/force-app/main/default/aura/skillEvaluationAndComents/skillEvaluationAndComentsController.js
@@ -2,7 +2,17 @@
     handleLikeButtonClick : function (cmp) {
         cmp.set('v.liked', !cmp.get('v.liked'));
     },
+
+    handleLikeButtonClick : function (cmp) {
+        cmp.set('v.liked', !cmp.get('v.liked'));
+    },
+    
     handleDislikeButtonClick: function (cmp) {
         cmp.set('v.disliked', !cmp.get('v.disliked'));
+    },
+
+    handleFavoriteButtonClick : function (cmp) {
+        cmp.set('v.favorite', !cmp.get('v.favorite'));
     }
+    
 });
